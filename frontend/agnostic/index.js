2b556e727f1be1186e7a197332033909fe4d749a
Gab <gabbybeaudry@gmail.com>
2019-05-09 00:25:51 -0400

    cleaned up code, added map

--- frontend/agnostic/index.js
+++ frontend/agnostic/index.js
@@ -1,3 +1,4 @@
+// keep json data in array 'mydata' 
 var mydata; 
 $.ajax({
   url: "https://data.austintexas.gov/resource/7d8e-dm7r.json",
@@ -10,20 +11,20 @@ $.ajax({
   console.log(data); // delete later 
 });
 
-// the DATA: an array of 5000, each item is a JSON object representing a row in the dataset. 
-// goal: show statistics on dockless usage for a given date range 
-// total # of trips
-// total # of miles 
-// total # unique units identified (might need to clarify this one)
-//var url = "https://data.austintexas.gov/resource/7d8e-dm7r.json"; 
-//var ss_div = document.getElementById('ss_div');  
 
+// create map for plotting mobility data 
+var map_austin = L.map('mapid').setView([30.2672, -97.7431], 13);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map_austin);
+
+// declare variables for use in summary statistic calculations
 var total_meters = 0; 
 var total_miles, total_trips, total_units = 0; 
-var temp_array = []; 
+var filtered_mydata = [];
 
 
-
+// functions to calculate summary statistics 
 // total number of miles
 function getTotalMiles() { 
   for (var i = 0; i <mydata.length; i++) { 
@@ -37,6 +38,12 @@ function getTotalMiles() {
 }
 getTotalMiles(); 
 
+ 
+
+
+
+
+
 // get total # of trips: count how many trip_ids there are 
 
 
@@ -45,32 +52,26 @@ getTotalMiles();
 
 
 
-
- function filterDate() {
-   var input_startDate = document.getElementById("input_startDate").value;
-   var input_endDate = document.getElementById("input_endDate").value; 
-   document.getElementById("dataparam_dates").innerHTML = input_startDate + " " + input_endDate;
- }
-
- function a { 
-  for (var i = 0; i <mydata.length; i++) { 
-    mydata[i]
-    if ((mydata[i].trip_distance != undefined)){ 
-      console.log('this is one trip_dist' + parseInt(mydata[i].trip_distance)); 
-      total_meters += parseInt(mydata[i].trip_distance); // consider adding || 0 to deal with NaN addition;  
+ function filterData() {
+   var startDate = new Date(document.getElementById("input_startDate").value);
+   var endDate = new Date(document.getElementById("input_endDate").value);
+   //var transport_mode = document.getElementById.apply("") 
+   document.getElementById("dataparam_dates").innerHTML = startDate + " " + endDate;
+  
+   for (var i = 0; i <mydata.length; i++) {
+    if ((mydata[i].start_time != undefined)){  
+     tripStartDate = new Date(mydata[i].start_time.substring(0,10)); //extract date from data and make a Date object 
+     tripEndDate = new Date(mydata[i].end_time.substring(0,10));
+     console.log(tripStartDate + tripEndDate); 
+    if  (tripStartDate.getTime() >= startDate.getTime()) { //&& (tripEndDate.getTime() <= endDate.getTime())) {
+      console.log(mydata[i]); 
+      filtered_mydata.push(mydata[i]); 
     }
-  }
-
+   }
+  } 
  }
 
 
-var map = L.map('mapid').setView([35, -98], 4);
-    
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 19,
-    attribution:  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd'
-}).addTo(map);
 
 
 
@@ -78,3 +79,10 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 
 
 
+ // the DATA: an array of 5000, each item is a JSON object representing a row in the dataset. 
+ // goal: show statistics on dockless usage for a given date range 
+ // total # of trips
+ // total # of miles 
+ // total # unique units identified (might need to clarify this one)
+ //var url = "https://data.austintexas.gov/resource/7d8e-dm7r.json"; 
+ //var ss_div = document.getElementById('ss_div');  
